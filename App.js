24c20a995126ee50ab3b52c9a1713d288d8f01d5
nbohane <nancybohane@gmail.com>
2022-09-14 10:59:51 -0400

    register screen ui

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import {LoginScreen} from '././src/components/screens/LoginScreen';
+import {RegisterScreen} from "./src/components/screens/RegisterScreen";
 
 export default function App() {
   return (
       <View >
-        <LoginScreen/>
+        <RegisterScreen/>
       </View>
 
   );
